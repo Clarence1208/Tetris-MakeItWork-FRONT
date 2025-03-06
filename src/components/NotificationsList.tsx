6e920390e4366355040e5352488092e8b59b6876
Loriane Hilderal <loriane.hilderal@canal-plus.com>
2025-03-06 15:47:51 +0100

    adding notifications list

---
+++ src/components/NotificationsList.tsx
@@ -0,0 +1,36 @@
+import {RiFlag2Line} from "react-icons/ri";
+
+type NotificationListProps = {
+    notifications: FlagNotification[];
+}
+export type FlagNotification = {
+    id: number;
+    title: string;
+    message: string;
+}
+
+export default function NotificationsList({notifications}: NotificationListProps) {
+
+    return (
+        <div id="notifications-list" className="list bg-base-100 rounded-box shadow-md">
+            {notifications.map((notification) => (
+               <Notification key={notification.id} notification={notification} />
+            ))}
+        </div>
+    )
+}
+
+type NotificationProps = {
+    notification: FlagNotification;
+}
+
+export function Notification({notification}: NotificationProps) {
+
+    return (
+        <div className="list-row">
+            <div>{notification.title}</div>
+            <div>{notification.message}</div>
+            <RiFlag2Line />
+        </div>
+    )
+}
