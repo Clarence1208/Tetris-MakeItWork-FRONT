15bf7f24c8fc9cb0d026b2909124c4aee53fcd6d
Victoires <94651953+Victoire-s@users.noreply.github.com>
2025-03-06 11:03:21 +0100

    feat(add): homepage init & router to homepage

---
+++ src/pages/home/Homepage.tsx
@@ -0,0 +1,12 @@
+import './homepage.css'
+
+function Homepage() {
+    return (
+        <>
+        <div className="test">
+            <h3>test 2</h3>
+        </div>
+        </>
+    )
+}
+export default Homepage;
