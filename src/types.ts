abb7b972f0b7383ec7bb22a43915da9bea19d901
Loriane Hilderal <loriane.hilderal@canal-plus.com>
2025-03-06 23:58:47 +0100

    adding modal for tasks

--- src/types.ts
+++ src/types.ts
@@ -14,7 +14,9 @@ export interface Point {
 
 export interface Task {
   id: string;
-  title: string;
+  name: string;
+  description: string;
+  company: string;
   skills: Skill[];
   shape: TetrisShape;
   status: KanbanStatus;
