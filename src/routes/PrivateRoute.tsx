6c5cdbb7262a86509be142e2fb1044affd1aad34
Victoires <94651953+Victoire-s@users.noreply.github.com>
2025-03-07 11:12:24 +0100

    feat(add): dashboard route

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Error404 from "../pages/error404/Error404.tsx";
 import Homepage from "../pages/home/Homepage.tsx";
 import { Skills } from "../pages/skills/Skills.tsx";
+import Dashboard from "../pages/dashboard/dashboard.tsx";
 
 const router = createBrowserRouter([
   { path: "/home", element: <Homepage /> },
   { path: "/skills", element: <Skills /> },
+  { path: "/dashboard", element: <Dashboard />}, 
   { path: "/", element: <Homepage /> },
   { path: "*", element: <Error404 /> },
 ]);
