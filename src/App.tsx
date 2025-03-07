a0289b9b72904b15d61e9a115385d0e6aadae401
Victoires <94651953+Victoire-s@users.noreply.github.com>
2025-03-07 11:12:47 +0100

    feat(modif): modifcation esthétique

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from "./components/Header.tsx";
 function App() {
     return (<>
             <Header />
-            <AppRouter></AppRouter>
+            <AppRouter />
         </>
     )
 }
