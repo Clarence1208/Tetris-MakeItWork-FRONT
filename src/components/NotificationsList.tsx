1d6511e0308d234e8fd57b691466174523b219a8
Loriane Hilderal <loriane.hilderal@canal-plus.com>
2025-03-07 01:09:42 +0100

    notifications seen handler

--- src/components/NotificationsList.tsx
+++ src/components/NotificationsList.tsx
@@ -1,5 +1,7 @@
 import {RiFlag2Line} from "react-icons/ri";
 import {User, Task} from "../types.ts"
+import CustomError from "./CustomError.tsx";
+import {useState} from "react";
 
 type NotificationListProps = {
     notifications: FlagNotification[];
@@ -31,11 +33,44 @@ type NotificationProps = {
 }
 
 export function Notification({notification}: NotificationProps) {
+
+    const [error, setError] = useState<string>("");
+    const [openError, setOpenError] = useState<boolean>(false);
+
+    function handleClose() {
+        setOpenError(false);
+    }
+    async function handleNotificationSeen(
+        notification: FlagNotification,
+    ){
+        try {
+            const API_URL = import.meta.env.VITE_API_URL;
+            const response = await fetch(`${API_URL}/notification/${notification.id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({isActive: false})
+            });
+
+            if (!response.ok) {
+                setError('Failed to create the task :' + response.statusText);
+                setOpenError(true);
+            }
+            const data: any = await response.json();
+            return data;
+        }catch (error) {
+            setError('Failed to create the task' + error);
+            setOpenError(true);
+            return {};
+        }
+    }
     return (
-        <div className="list-row">
+        <div className="list-row" onClick={() => handleNotificationSeen}>
+            {openError && <CustomError message={error} handleClose={handleClose}/>}
             <div>{notification.taskId.skills[0]}</div>
             <div>{notification.senderId.name} asked for your help</div>
-            <RiFlag2Line />
+            <RiFlag2Line fill={notification.isActive ? "green" : "red"} />
         </div>
     )
 }
